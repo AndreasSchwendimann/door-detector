4d95568dad9cb3b7ed4231f129c557cb6f5c8933
Andreas Schwendimann <andreas@zeno.ch>
2020-03-13 15:30:30 +0100

    update script for use on RPzero

--- trigger.py
+++ trigger.py
@@ -1,64 +1,28 @@
-# Libraries
-import RPi.GPIO as GPIO
-import time
-
-# GPIO Mode (BOARD / BCM)
-GPIO.setmode(GPIO.BCM)
-
-# set GPIO Pins
-GPIO_TRIGGER = 7
-GPIO_ECHO = 11
-
-# set GPIO direction (IN / OUT)
-GPIO.setup(GPIO_TRIGGER, GPIO.OUT)
-GPIO.setup(GPIO_ECHO, GPIO.IN)
+from gpiozero import DistanceSensor
+from time import sleep
 
 event_url = 'www.panda-cuddles.ch/api/Event'
 
-
 def get_distance():
-    # set Trigger to HIGH
-    GPIO.output(GPIO_TRIGGER, True)
+    sensor = DistanceSensor(echo=11, trigger=7)
+    return (sensor.distance * 100)
 
-    # set Trigger after 0.01ms to LOW
-    time.sleep(0.00001)
-    GPIO.output(GPIO_TRIGGER, False)
-
-    StartTime = time.time()
-    StopTime = time.time()
-
-    # save StartTime
-    while GPIO.input(GPIO_ECHO) == 0:
-        StartTime = time.time()
-
-    # save time of arrival
-    while GPIO.input(GPIO_ECHO) == 1:
-        StopTime = time.time()
-
-    # time difference between start and arrival
-    TimeElapsed = StopTime - StartTime
-    # multiply with the sonic speed (34300 cm/s)
-    # and divide by 2, because there and back
-    distance = (TimeElapsed * 34300) / 2
-
-    return distance
-
-button_pressed = False
-button_pressed_before = False
+door_status = False
+door_status_before = False
 
 while True:
     distance = get_distance()
     if distance <= 1:
         print("++ button was pressed")
-        button_pressed = True
+        door_status = True
     else:
         print("-- no button was pressed")
-        button_pressed = False
+        door_status = False
 
-    if button_pressed and not button_pressed_before:
+    if door_status and not door_status_before:
         requests.post(event_url, json={'created': str(datetime.datetime.now()), 'location': 'office 1', 'action': 'open'})
-    if not button_pressed and button_pressed_before:
+    if not door_status and door_status_before:
         requests.post(event_url, json={'created': str(datetime.datetime.now()), 'location': 'office 1', 'action': 'close'})
 
-    button_pressed_before = button_pressed
-    time.sleep(1)
+    door_status_before = door_status
+    sleep(1)
